5950701d0fe33cfe04e024b2b9c8167df1073fe9
tomohiro tanaka <tomohiro.tanaka27@gmail.com>
2024-05-31 14:47:49 +0900

    add contract MyConduitController, MySeaport

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,21 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: {
+    compilers: [
+      { version: "0.8.19" },
+      {
+        version: "0.8.17",
+        settings: {
+          viaIR: true,
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
+      },
+    ],
+  },
   typechain: {
     outDir: "frontend/types",
     target: "ethers-v6",
